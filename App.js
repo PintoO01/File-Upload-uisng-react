fa0874e5db19178fea191dccd399b5f184ca931a
PintoO01 <53212095+PintoO01@users.noreply.github.com>
2021-06-01 17:16:39 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+/**q
+ * Component to handle file upload. Works for image
+ * uploads, but can be edited to work for any file.
+ */
+
+function FileUpload() {
+  
+  // State to store uploaded file
+  const [file, setFile] = React.useState("");
+  const[description,setDescription]=useState("")
+  console.log(description,file)  
+
+  // Handles file upload event and updates state
+  function handleUpload(event) {
+    setFile(event.target.files[0]);
+    // Add code here to upload file to server
+    // ...
+  }
+  
+
+  return (
+    
+    <div id="upload-box">
+      <h1>
+       File upload using react
+      </h1>
+      <input type="file" onChange={handleUpload} />
+      <p>Filename: {file.name}</p>
+      <p>File type: {file.type}</p>
+      <p>File size: {file.size} bytes</p>
+      <p>File description: {file.description}</p>
+
+      {file && <ImageThumb image={file} />}
+    </div>
+  );
+}
+
+/**
+ * Component to display thumbnail of image.
+ */
+const ImageThumb = ({ image }) => {
+  return <img src={URL.createObjectURL(image) } alt={image.name} />;
+  
+};
+
+export default function App() {
+  
+  return <FileUpload />;
+}
